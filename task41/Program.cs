b18c3cd87c59df570812da1f930c570ec98675a0
pavelkulak <kulak-pavel@yandex.ru>
2023-07-27 22:37:50 +0400

    complete

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = CountPositiveNumbers();
+Console.WriteLine($"Количество чисел больше 0: {count}");
+
+
+static int CountPositiveNumbers()
+{
+    Console.WriteLine("Введите числа (для завершения ввода введите любой символ, не являющийся числом):");
+    int count = 0;
+
+    while (true)
+    {
+        int number;
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            if (number > 0)
+            {
+                count++; // Увеличиваем счетчик, если число больше 0
+            }
+        }
+        else
+        {
+            break; // Прерываем цикл, если ввод не является числом
+        }
+    }
+
+    return count;
+}
---
+++ task43/Program.cs
@@ -0,0 +1,18 @@
+﻿double b1 = GetDouble("Введите значение b1: ");
+double k1 = GetDouble("Введите значение k1: ");
+double b2 = GetDouble("Введите значение b2: ");
+double k2 = GetDouble("Введите значение k2: ");
+
+// Находим точку пересечения
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения: ({x}; {y})");
+
+
+static double GetDouble(string message)
+{
+    Console.WriteLine(message);
+    double num = double.Parse(Console.ReadLine());
+    return num;
+}
